89614ca512506d81a49523c18014f208d37823b0
Rinaldi Jandrinata <puppet13th@gmail.com>
2019-03-15 15:16:55 +0700

    added dwc2 to modules

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo placing files...
-cp g_ether.conf /etc/modprobe.d/g_ether.conf
-cp usb-gadget-init /sbin/usb-gadget-init
-cp usb-gadget.service /etc/systemd/system/usb-gadget.service
+cp -f g_ether.conf /etc/modprobe.d/g_ether.conf
+cp -f usb-gadget-init /sbin/usb-gadget-init
+cp -f usb-gadget.service /etc/systemd/system/usb-gadget.service
 chmod +x /sbin/usb-gadget-init
 
 echo updating system configuration...
 cat /boot/config.txt | grep -q "dtoverlay=dwc2" || echo "dtoverlay=dwc2" >> /boot/config.txt
+cat /etc/modules | grep -q "dwc2" || echo "dwc2" >> /etc/modules
 cat /etc/modules | grep -q "g_zero" || echo "g_zero" >> /etc/modules
 
 echo enabling services...
