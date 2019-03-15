11d84a20c187573004086cabb081b519b1d2b729
Rinaldi Jandrinata <48564964+puppet13th@users.noreply.github.com>
2019-03-15 15:06:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo placing files...
+cp g_ether.conf /etc/modprobe.d/g_ether.conf
+cp usb-gadget-init /sbin/usb-gadget-init
+cp usb-gadget.service /etc/systemd/system/usb-gadget.service
+chmod +x /sbin/usb-gadget-init
+echo updating system configuration...
+cat /boot/config.txt | grep -q "dtoverlay=dwc2" || echo "dtoverlay=dwc2" >> /boot/config.txt
+cat /etc/modules | grep -q "g_zero" || echo "g_zero" >> /etc/modules
+echo enabling services...
+systemctl enable usb-gadget.service
+systemctl enable getty@ttyGS0.service
+echo starting services...
+systemctl enable usb-gadget.service
+systemctl enable getty@ttyGS0.service
