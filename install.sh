acf579c7c4d063483175962d932e9337e7ad2264
Rinaldi Jandrinata <48564964+puppet13th@users.noreply.github.com>
2019-03-15 19:04:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
 echo placing files...
-cp -f g_ether.conf /etc/modprobe.d/g_ether.conf
-cp -f usb-gadget-init /usr/bin/usb-gadget-init
-mkdir -p /usr/lib/systemd/system && cp -f usb-gadget.service /usr/lib/systemd/system/usb-gadget.service
-chmod +x /usr/bin/usb-gadget-init
+cmp -s g_ether.conf /etc/modprobe.d/g_ether.conf
+if [ $? -eq 1 ]
+  then
+  echo updating /etc/modprobe.d/g_ether.conf
+  cp -f g_ether.conf /etc/modprobe.d/g_ether.conf
+fi
+cmp -s usb-gadget-init /usr/bin/usb-gadget-init
+if [ $? -eq 1 ]
+  then
+  echo updating /usr/bin/usb-gadget-init
+  cp -f usb-gadget-init /usr/bin/usb-gadget-init
+fi
+cmp -s usb-gadget.service /usr/lib/systemd/system/usb-gadget.service
+if [ $? -eq 1 ]
+  then
+  echo updating /usr/lib/systemd/system/usb-gadget.service
+  mkdir -p /usr/lib/systemd/system && cp -f usb-gadget.service /usr/lib/systemd/system/usb-gadget.service
+  chmod +x /usr/bin/usb-gadget-init
+fi
 
 echo updating system configuration...
-cat /boot/config.txt | grep -q "dtoverlay=dwc2" || echo "dtoverlay=dwc2" >> /boot/config.txt
-cat /etc/modules | grep -q "dwc2" || echo "dwc2" >> /etc/modules
-cat /etc/modules | grep -q "g_zero" || echo "g_zero" >> /etc/modules
+grep -q "dtoverlay=dwc2" /boot/config.txt|| {
+echo "dtoverlay=dwc2" >> /boot/config.txt
+rebootneed=1
+}
+grep -q "dwc2" /etc/modules|| echo "dwc2" >> /etc/modules
 
 echo enabling services...
 systemctl enable usb-gadget.service
-systemctl enable getty@ttyGS0.service
-
-echo starting services...
-systemctl enable usb-gadget.service
-systemctl enable getty@ttyGS0.service
+if [ ! $rebootneed -eq 1 ]
+  then
+  echo type "systemctl enable usb-gadget.service" to enable service
+  else
+  echo "system reboot required!"
+fi
